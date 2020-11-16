350e95cb582c867c76016cb2f8de09b39e50dc8f
devlexanderxyz <infinityen@protonmail.com>
2020-11-16 13:14:49 +0000

    add eslint, convert from mjs to js, add types

---
+++ server/index.js
@@ -0,0 +1,31 @@
+/// <reference types="@altv/types-server" />
+import * as alt from "alt-server";
+
+const options = {
+	"Kick": {
+		execute: (player) => player.kick()
+	},
+	"Kill": {
+		execute: (player) => player.health = 0
+	}
+};
+
+alt.onClient("playersList:getOptions", (player) => {
+	alt.emitClient(player, "playersList:setOptions", Object.keys(options));
+});
+
+alt.onClient("playersList:getPlayers", (player) => {
+	alt.emitClient(player, "playersList:setPlayers", alt.Player.all.map(player => ({
+		id: player.id,
+		name: player.name,
+		ping: player.ping
+	})));
+});
+
+alt.onClient("playersList:optionExecute", (_, id, optionName) => {
+	const player = alt.Player.all.find(player => player.id === id);
+
+	if (player) {
+		options[optionName].execute(player);
+	}
+});
